8273bb6445034380fff847f98b20dc88e0bd7653
Tang <tanglove4077@gmail.com>
2026-06-29 01:15:00 +0700

    สร้างหน้า Git Commands

--- js/script.js
+++ js/script.js
@@ -16,4 +16,42 @@ githubBtn.addEventListener("click", function () {
 
     alert("บทต่อไปเราจะเชื่อมไปยัง GitHub");
 
-});
+});const darkButton = document.getElementById("darkMode");
+
+darkButton.addEventListener("click", function(){
+
+document.body.classList.toggle("dark");
+
+});function openVSCode(){
+
+window.open("https://code.visualstudio.com","_blank");
+
+}
+
+function openGit(){
+
+window.open("https://git-scm.com","_blank");
+
+}
+
+function openNode(){
+
+window.open("https://nodejs.org","_blank");
+
+}function copyCommand(id) {
+
+    const text = document.getElementById(id).textContent;
+
+    navigator.clipboard.writeText(text);
+
+    alert("คัดลอกแล้ว : " + text);
+
+}function copyCommand(id){
+
+    const text = document.getElementById(id).textContent;
+
+    navigator.clipboard.writeText(text);
+
+    alert("คัดลอกแล้ว\n\n" + text);
+
+}
