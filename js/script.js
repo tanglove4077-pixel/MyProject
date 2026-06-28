bcc7fd67bee23e1421340f12c6e73115546920ee
Tang <tanglove4077@gmail.com>
2026-06-29 00:57:35 +0700

    สร้างโครงสร้างเว็บไซต์ใหม่

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// แสดงข้อความเมื่อเว็บโหลดเสร็จ
+
+console.log("Welcome to VS Code Learning Hub");
+
+// หา Button
+
+const button = document.getElementById("btnStart");
+
+// เมื่อกดปุ่ม
+
+button.addEventListener("click", function(){
+
+    alert("ยินดีต้อนรับเข้าสู่บทเรียน VS Code");
+
+});
